04214b0f10b44d4fd216d859d9090af560768e8f
Mark Brady <mbrady@yelp.com>
2014-10-03 17:16:16 -0700

    More commit action

--- src/timezone.c
+++ src/timezone.c
@@ -6,3 +6,14 @@ int main(void) {
   app_event_loop();
   watchface_deinit();
 }
+
+struct tm getTime(struct tm *tick_time) {
+
+  //if (!tick_time) {
+    time_t now = time(NULL);
+    now = now + 1 * 60; // 1 hour in seconds.
+    //now = now;
+    tick_time = localtime(&now);
+  //}
+  return *tick_time;
+}
--- src/watchface.c
+++ src/watchface.c
@@ -5,23 +5,49 @@ static Window *window;
 static TextLayer *text_date_layer;
 static TextLayer *text_time_layer;
 
-static void line_layer_update_callback(Layer *layer, GContext* ctx) {
-  graphics_context_set_fill_color(ctx, GColorWhite);
-  graphics_fill_rect(ctx, layer_get_bounds(layer), 0, GCornerNone);
-}
-
 static void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
-  // Need to be static because they're used by the system later.
-  static char time_text[] = "00:00";
-  static char date_text[] = "Xxxxxxxxx 00";
+    // Need to be static because they're used by the system later.
+    static char time_text[] = "00:00";
+    static char date_text[] = "Xxxxxxxxx 00";
 
-  char *time_format;
+    char *time_format;
 
-  if (!tick_time) {
-    time_t now = time(NULL);
-    tick_time = localtime(&now);
-  }
+    if (!tick_time) {
+      APP_LOG(APP_LOG_LEVEL_DEBUG, "Update the tick time from local time");
+      time_t now = time(NULL);
+      //now += 100000;  __darwin_time_t
+      tick_time = localtime(&now);
+    }
+    tick_time->tm_hour = 0;
+    
+    time_t nowTime = time(NULL);
 
+    //long nowLong = nowTime.__darwin_time_t;
+    //APP_LOG(APP_LOG_LEVEL_DEBUG, "local time %l", nowLong);
+    
+    int	tm_sec = tick_time->tm_sec;
+    int	tm_min = tick_time->tm_min;
+    int	tm_hour = tick_time->tm_hour;
+    int	tm_mday = tick_time->tm_mday;
+    int	tm_mon = tick_time->tm_mon;
+    int	tm_year = tick_time->tm_year;
+    int	tm_wday = tick_time->tm_wday;
+    int	tm_yday = tick_time->tm_yday;
+    int tm_isdst = tick_time->tm_isdst;
+
+    
+
+    //long tm_gmtoff = tick_time->tm_gmtoff;
+    //char *tm_zone = tick_time->tm_zone;
+
+    APP_LOG(
+            APP_LOG_LEVEL_DEBUG,
+            "tick_time hour %d  min %d",
+            tm_hour,
+            tm_min
+            );
+    
+    
   // TODO: Only update the date when it's changed.
   strftime(date_text, sizeof(date_text), "%B %e", tick_time);
   text_layer_set_text(text_date_layer, date_text);
